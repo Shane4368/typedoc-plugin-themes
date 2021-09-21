dbb05b2bc363b9702dbdc03ad728938663839f81
Shane4368 <shane4368.dev@gmail.com>
2021-09-20 23:31:31 -0500

    Apply themes to root element
    * Delete themes.js

--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ const { PageEvent } = require("typedoc");
  */
 function load(app)
 {
-    const themeJs = fs.readFileSync(__dirname + "/themes.js", "utf-8");
     const themeCss = fs.readFileSync(__dirname + "/themes.css", "utf-8");
     const themeOptions = fs.readFileSync(__dirname + "/themes.xml", "utf-8");
 
     app.renderer.on(PageEvent.END, (/**@type {PageEvent}*/ page) =>
     {
         const html = page.contents.replace(/(<option value="dark">Dark<\/option>)/, "$1" + themeOptions);
-        page.contents = html.replace(/(<\/head>)/, `<style>${themeCss}</style><script>${themeJs}</script>$1`);
+        page.contents = html.replace(/(<\/head>)/, `<style>${themeCss}</style>$1`);
     });
 }
 
